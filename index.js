bf5c8baf7ae67b941ac7cbd8b8bafdca79696c92
Abieyra <163407565+Abieyra@users.noreply.github.com>
2024-05-13 13:41:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+// declare const ans and assign it to the result  of inquirer.prompt
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "string",
+        message: "Enter your sentence to count the word"
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+// print arrey of words to the console
+console.log(words);
+// print the word count of the sentence to the console
+console.log(`your sentence word count is ${words.length}`);
